243345047ddfaa5a1587db17c2516907c63102cc
ixsluo <xiaoshan.luo@outlook.com>
2023-03-20 09:15:07 +0800

    feat(build_mlp): add dropout option to build_mlp, change ReduceLROnPlateau min_lr to 0
    Signed-off-by: ixsluo <xiaoshan.luo@outlook.com>

--- cdvae/pl_modules/basic_blocks.py
+++ cdvae/pl_modules/basic_blocks.py
@@ -1,9 +1,13 @@
 import torch.nn as nn
 
 
-def build_mlp(in_dim, hidden_dim, fc_num_layers, out_dim):
+def build_mlp(
+    in_dim, hidden_dim, fc_num_layers, out_dim, dropout=False, drop_ratio=0.5
+):
     mods = [nn.Linear(in_dim, hidden_dim), nn.ReLU()]
     for i in range(fc_num_layers - 1):
         mods += [nn.Linear(hidden_dim, hidden_dim), nn.ReLU()]
+        if dropout:
+            mods += [nn.Dropout(drop_ratio)]
     mods += [nn.Linear(hidden_dim, out_dim)]
     return nn.Sequential(*mods)
