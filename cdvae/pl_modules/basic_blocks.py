d2fb6102b5fd9cd9f17fb527396a5c6d946c85ee
ixsluo <xiaoshan.luo@outlook.com>
2023-05-09 09:32:05 +0800

    feat: add optional lazylinear in build_mlp
    Signed-off-by: ixsluo <xiaoshan.luo@outlook.com>

--- cdvae/pl_modules/basic_blocks.py
+++ cdvae/pl_modules/basic_blocks.py
@@ -2,7 +2,10 @@ import torch.nn as nn
 
 
 def build_mlp(in_dim, hidden_dim, fc_num_layers, out_dim, dropout=0.):
-    mods = [nn.Linear(in_dim, hidden_dim), nn.ReLU()]
+    if in_dim is None:
+        mods = [nn.LazyLinear(hidden_dim), nn.ReLU()]
+    else:
+        mods = [nn.Linear(in_dim, hidden_dim), nn.ReLU()]
     for i in range(fc_num_layers - 1):
         mods += [nn.Linear(hidden_dim, hidden_dim), nn.ReLU()]
         mods += [nn.Dropout(dropout)]
