19072a578dacdb29ed9a613aad79cce0654d423e
ixsluo <xiaoshan.luo@outlook.com>
2023-08-15 14:57:47 +0800

    fix: vasp2dataset on OUTCAR directly
    Signed-off-by: ixsluo <xiaoshan.luo@outlook.com>

--- scripts/vasp2dataset.py
+++ scripts/vasp2dataset.py
@@ -31,13 +31,13 @@ def read_outcar(outcar):
             if "enthalpy is  TOTEN" in line:
                 enthalpy = float(line.strip().split()[4])
                 break
-    # _id = Path(outcar).stem[7:]  # OUTCAR_*.vasp
-    _id = Path(outcar).parent.stem + '.vasp'  # *.run/OUTCAR_ -> *.vasp
+    _id = Path(outcar).stem  # *.OUTCAR_ -> *
     return {
             "material_id": _id,
             "formula": atoms.get_chemical_formula("metal"),
-            "nsites": len(atoms),
+            "natoms": len(atoms),
             "volume": atoms.get_volume(),
+            "volume_per_atom": atoms.get_volume() / len(atoms),
             # "energy": energy,
             # "energy_per_atom": energy / len(atoms),
             "enthalpy": enthalpy,
@@ -51,7 +51,7 @@ def read_outcar(outcar):
 def main(dir_list):
     ser_list = []
     for d in dir_list:
-        for f in tqdm(Path(d).glob("*.run/OUTCAR")):
+        for f in tqdm(Path(d).glob("*.OUTCAR")):
             data_dict = read_outcar(f)
             if data_dict:
                 ser = pd.Series(data_dict)
@@ -61,6 +61,7 @@ def main(dir_list):
 
 
 if __name__ == "__main__":
+    # <dir>/*.OUTCAR
     dir_list = sys.argv[1:]
     df = main(dir_list)
     df.to_feather("opt.feather")
