a312e23e31a2f702a4dfac793670adf4c135cfee
ixsluo <xiaoshan.luo@outlook.com>
2023-09-01 20:25:43 +0800

    feat: find unique structures in gen list
    Signed-off-by: ixsluo <xiaoshan.luo@outlook.com>

---
+++ scripts/statgen_uniq.py
@@ -0,0 +1,80 @@
+# find unique structures in a given dir
+
+import pickle
+from pathlib import Path
+
+import click
+import pandas as pd
+from joblib import Parallel, delayed
+from pymatgen.analysis.structure_matcher import StructureMatcher
+from pymatgen.core.structure import Structure
+from tqdm import tqdm
+
+
+def get_matchers():
+    matcher_lo = StructureMatcher(ltol=0.3, stol=0.5, angle_tol=10)  # loose
+    matcher_md = StructureMatcher(ltol=0.2, stol=0.3, angle_tol=5)  # midium
+    matcher_st = StructureMatcher(ltol=0.1, stol=0.2, angle_tol=5)  # strict
+    matchers = {
+        "matcher_lo": matcher_lo,
+        "matcher_md": matcher_md,
+        "matcher_st": matcher_st,
+    }
+    return matchers
+
+
+def load_uniq_dict(picklefile):
+    if Path(picklefile).exists():
+        with open(picklefile, "rb") as f:
+            uniq_dict = pickle.load(f)
+    else:
+        uniq_dict = {}
+    return uniq_dict
+
+
+def get_uniq_df(gendir, matchers):
+    gen_list = sorted(
+        [f.stem for f in gendir.joinpath("gen").glob("*.vasp")], key=lambda i: int(i)
+    )
+    genst_dict = {
+        genfn.stem: Structure.from_file(genfn)
+        for genfn in [gendir / f"gen/{i}.vasp" for i in gen_list]
+    }
+    df = pd.DataFrame(True, gen_list, list(matchers.keys()))
+    for mat_name, matcher in matchers.items():
+        uniqid = []
+        for fi in gen_list:
+            for uid in uniqid:
+                if matcher.fit(genst_dict[fi], genst_dict[uid]):
+                    df.loc[fi, mat_name] = False
+                    break
+            else:
+                uniqid.append(fi)
+    return df
+
+
+@click.command
+@click.argument("gendir", nargs=-1)
+@click.option(
+    "-p",
+    "--picklefile",
+    default="uniq_dict.pkl",
+    help="out pickle file to update, default uniq_dict.pkl",
+)
+@click.option("-j", "--njobs", default=-1, help="default: -1")
+def filter_uniq(gendir, picklefile, njobs):
+    matchers = get_matchers()
+    gendir = [Path(d) for d in gendir if Path(d).is_dir()]
+
+    mpnames = [d.name[9:] for d in gendir]
+    uniqdflist = Parallel(njobs, backend="multiprocessing")(
+        delayed(get_uniq_df)(d, matchers) for d in tqdm(gendir)
+    )
+    uniq_dict = {mpname: df for mpname, df in zip(mpnames, uniqdflist)}
+    uniq_dict = load_uniq_dict(picklefile).update(uniq_dict)
+    with open(picklefile, "wb") as f:
+        pickle.dump(uniq_dict, f)
+
+
+if __name__ == '__main__':
+    filter_uniq()
