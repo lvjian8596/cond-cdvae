d4b0d4831f4edc6ac92727bd985f3376522c44d3
ixsluo <xiaoshan.luo@outlook.com>
2023-09-13 20:59:13 +0800

    feat: add parallel to prepare_vasp
    Signed-off-by: ixsluo <xiaoshan.luo@outlook.com>

--- scripts/prepare_vasp.py
+++ scripts/prepare_vasp.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 import click
+from joblib import Parallel, delayed
 from pymatgen.core.structure import Structure
 from pymatgen.io.vasp import VaspInput
 from pymatgen.io.vasp.sets import MPRelaxSet
@@ -8,7 +9,7 @@ from tqdm import tqdm
 
 
 def prepare_task(structure, relax_path, PSTRESS, NSW):
-    user_incar_settings={
+    user_incar_settings = {
         'NSW': NSW,
         'LREAL': False,
         'ISMEAR': 0,
@@ -35,19 +36,26 @@ def prepare_task(structure, relax_path, PSTRESS, NSW):
     vasp.write_input(relax_path)
 
 
+def wrapped_prepare_task(indir, sf, nsw, pstress):
+    runtype = ".scf" if nsw <= 1 else ".opt"
+    structure = Structure.from_file(sf)
+    relax_path = indir.with_suffix(runtype).joinpath(sf.stem)
+    relax_path.mkdir(exist_ok=True, parents=True)
+    prepare_task(structure, relax_path, pstress, nsw)
+
+
 @click.command
 @click.argument("indir")
 @click.option("-s", "--nsw", default=0, help="NSW, default 0")
 @click.option("-p", "--pstress", default=0, help="PSTRESS (kbar), default 0")
-def prepare_batch(indir, nsw: int, pstress: float):
+@click.option("-j", "--njobs", default=-1, type=int)
+def prepare_batch(indir, nsw: int, pstress: float, njobs: int):
     click.echo(f"You are using {nsw=} {pstress=}")
     indir = Path(indir)
-    runtype = ".scf" if nsw <= 1 else ".opt"
-    for sf in tqdm(list(indir.glob("*.vasp")) + list(indir.glob("*.cif"))):
-        structure = Structure.from_file(sf)
-        relax_path = indir.with_suffix(runtype).joinpath(sf.stem)
-        relax_path.mkdir(exist_ok=True, parents=True)
-        prepare_task(structure, relax_path, pstress, nsw)
+    Parallel(njobs, backend="multiprocessing")(
+        delayed(wrapped_prepare_task)(indir, sf, nsw, pstress)
+        for sf in tqdm(list(indir.glob("*.vasp")) + list(indir.glob("*.cif")))
+    )
 
 
 if __name__ == '__main__':
