e98cb598f744f0082283565a0f0ba610100f436a
ixsluo <xiaoshan.luo@outlook.com>
2023-09-14 14:05:07 +0800

    feat: filter uniq in prepare vasp
    Signed-off-by: ixsluo <xiaoshan.luo@outlook.com>

--- scripts/prepare_vasp.py
+++ scripts/prepare_vasp.py
@@ -1,3 +1,4 @@
+import warnings
 from pathlib import Path
 
 import click
@@ -5,6 +6,7 @@ from joblib import Parallel, delayed
 from pymatgen.core.structure import Structure
 from pymatgen.io.vasp import VaspInput
 from pymatgen.io.vasp.sets import MPRelaxSet
+from statgen import read_format_table
 from tqdm import tqdm
 
 
@@ -21,12 +23,14 @@ def prepare_task(structure, relax_path, PSTRESS, NSW):
     if NSW > 1:
         user_incar_settings["ISYM"] = 0
 
-    mp_set = MPRelaxSet(
-        structure,
-        user_incar_settings=user_incar_settings,
-        user_potcar_settings={"W": "W_sv"},
-        user_potcar_functional="PBE_54",
-    )
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        mp_set = MPRelaxSet(
+            structure,
+            user_incar_settings=user_incar_settings,
+            user_potcar_settings={"W": "W_sv"},
+            user_potcar_functional="PBE_54",
+        )
     vasp = VaspInput(
         incar=mp_set.incar,
         kpoints=mp_set.kpoints,
@@ -36,11 +40,14 @@ def prepare_task(structure, relax_path, PSTRESS, NSW):
     vasp.write_input(relax_path)
 
 
-def wrapped_prepare_task(indir, sf, nsw, pstress):
+def wrapped_prepare_task(indir, uniq, uniqlevel, sf, nsw, pstress):
     runtype = ".scf" if nsw <= 1 else ".opt"
-    structure = Structure.from_file(sf)
+    if uniq is not None:
+        runtype = f".uniq.{uniqlevel}" + runtype
     relax_path = indir.with_suffix(runtype).joinpath(sf.stem)
     relax_path.mkdir(exist_ok=True, parents=True)
+
+    structure = Structure.from_file(sf)
     prepare_task(structure, relax_path, pstress, nsw)
 
 
@@ -48,13 +55,32 @@ def wrapped_prepare_task(indir, sf, nsw, pstress):
 @click.argument("indir")
 @click.option("-s", "--nsw", default=0, help="NSW, default 0")
 @click.option("-p", "--pstress", default=0, help="PSTRESS (kbar), default 0")
+@click.option("-u", "--uniq", default=None, help="unique file, default None")
+@click.option(
+    "-l",
+    "--uniqlevel",
+    default="lo",
+    type=click.Choice(["lo", "md", "st"]),
+    help="unique level of matcher",
+)
 @click.option("-j", "--njobs", default=-1, type=int)
-def prepare_batch(indir, nsw: int, pstress: float, njobs: int):
+def prepare_batch(
+    indir, nsw: int, pstress: float, njobs: int, uniq=None, uniqlevel="lo"
+):
     click.echo(f"You are using {nsw=} {pstress=}")
     indir = Path(indir)
+    flist = list(indir.glob("*.vasp"))
+    if uniq is not None:
+        lv = f"matcher_{uniqlevel}"
+        uniqdf = read_format_table(uniq)
+        if lv not in uniqdf.columns:
+            raise KeyError(f"key '{uniqlevel}' not in {uniq}")
+        click.echo(f"using unique key '{lv}' in {uniq}")
+        uniqlist = list(uniqdf[uniqdf[lv]].index)
+        flist = [fi for fi in flist if int(fi.stem) in uniqlist]
     Parallel(njobs, backend="multiprocessing")(
-        delayed(wrapped_prepare_task)(indir, sf, nsw, pstress)
-        for sf in tqdm(list(indir.glob("*.vasp")) + list(indir.glob("*.cif")))
+        delayed(wrapped_prepare_task)(indir, uniq, uniqlevel, sf, nsw, pstress)
+        for sf in tqdm(flist, ncols=120)
     )
 
 
