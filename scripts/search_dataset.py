1360e33693d833f53d425b90e1199c1b10e92756
ixsluo <xiaoshan.luo@outlook.com>
2023-09-15 14:32:37 +0800

    feat: add spgno in search dataset
    Signed-off-by: ixsluo <xiaoshan.luo@outlook.com>

--- scripts/search_dataset.py
+++ scripts/search_dataset.py
@@ -6,13 +6,20 @@ import pandas as pd
 @click.argument("dataset")
 @click.option("-f", "--formula", help="query formula")
 @click.option("-i", "--material_id", help="query material_id")
-def search(dataset, formula=None, material_id=None):
+@click.option("--spgno", type=int, help="spgno")
+def search(dataset, formula=None, material_id=None, spgno=None):
     click.echo(dataset)
     ds = pd.read_feather(dataset)
     if formula is not None:
-        click.echo(ds[ds.formula == formula])
+        if spgno is not None:
+            click.echo(ds[(ds.formula == formula) & (ds.spgno == spgno)])
+        else:
+            click.echo(ds[ds.formula == formula])
     if material_id is not None:
-        click.echo(ds[ds.material_id == material_id])
+        if spgno is not None:
+            click.echo(ds[(ds.material_id == material_id) & (ds.spgno == spgno)])
+        else:
+            click.echo(ds[ds.material_id == material_id])
 
 
 if __name__ == "__main__":
