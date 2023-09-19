3fd8bc4c1ab351806411d77e92d8fc0536e294b5
ixsluo <xiaoshan.luo@outlook.com>
2023-09-19 13:35:17 +0800

    feat: read formula from mpds in split_mpexpe
    Signed-off-by: ixsluo <xiaoshan.luo@outlook.com>

--- scripts/split_mpexpe.py
+++ scripts/split_mpexpe.py
@@ -20,10 +20,10 @@ import pandas as pd
 def split_dataset(ds, mpds, name, nperpart):
     ds = pd.read_feather(ds)
     mpds = pd.read_feather(mpds)
-    theo_mpds = mpds[mpds.theoretical == True][["material_id"]]
-    expe_mpds = mpds[mpds.theoretical != True][["material_id"]]
-    theo_ds = pd.merge(ds, theo_mpds, on="material_id")
-    expe_ds = pd.merge(ds, expe_mpds, on="material_id")
+    theo_mpds = mpds[mpds.theoretical][["material_id", "formula"]]
+    expe_mpds = mpds[~mpds.theoretical][["material_id", "formula"]]
+    theo_ds = pd.merge(ds, theo_mpds, on="material_id", suffixes=("_x", ""))
+    expe_ds = pd.merge(ds, expe_mpds, on="material_id", suffixes=("_x", ""))
     dsname = {"mptheo": theo_ds, "mpexpe": expe_ds}
     for ipart in range(len(dsname[name]) // nperpart + 1):
         dspart = dsname[name][ipart * nperpart : (ipart + 1) * nperpart]
@@ -43,7 +43,11 @@ if __name__ == "__main__":
 
 # batch run generation
 """
-python split_dataset.py --ds /home/share/Data/Caly-MP/230617/v2_std_plus_DFTLi/test.feather --nperpart 600
+python split_dataset.py \
+    --ds /home/share/Data/Caly-MP/230617/v2_std_plus_DFTLi/test.feather \
+    --nperpart 600
 # >>> mpexpe_p{0..5}.txt
-awk '{print "--label " $1 " --formula " $2 " --pressure " $3 }' mpexpe_p2.txt|xargs -n 6 python ~/cond-cdvae/scripts/evaluate.py --model_path `pwd` --batch_size 100 --num_batches_to_samples 2 --tasks gen
+awk '{print "--label " $1 " --formula " $2 " --pressure " $3 }' mpexpe_p2.txt| \
+    xargs -n 6 python ~/cond-cdvae/scripts/evaluate.py \
+    --model_path `pwd` --batch_size 100 --num_batches_to_samples 2 --tasks gen
 """
