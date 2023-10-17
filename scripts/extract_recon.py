596aa5ffd45837c4b31eb0314a2498c803537365
ixsluo <xiaoshan.luo@outlook.com>
2023-10-17 21:33:02 +0800

    feat: add custom extract_recon
    Signed-off-by: ixsluo <xiaoshan.luo@outlook.com>

--- scripts/extract_recon.py
+++ scripts/extract_recon.py
@@ -11,37 +11,71 @@ from ase.io import write
 from tqdm import tqdm
 
 
+def extract_recon_pt(recon_pt_file: Path):
+    extract_dir = recon_pt_file.with_name(recon_pt_file.stem)
+    pkl = extract_dir.joinpath(recon_pt_file.name + ".pkl")
+    idx = extract_dir.joinpath(recon_pt_file.name + ".idx")
+    atoms_dir = extract_dir.joinpath(recon_pt_file.name + ".dir")
+    atoms_dir.mkdir(exist_ok=True, parents=True)
+
+    recon = torch.load(recon_pt_file)
+
+    from eval_utils import get_crystals_list  # change working dir
+
+    batch_idx = 0
+    crys_array_list = get_crystals_list(
+        recon["frac_coords"][batch_idx],
+        recon["atom_types"][batch_idx],
+        recon["lengths"][batch_idx],
+        recon["angles"][batch_idx],
+        recon["num_atoms"][batch_idx],
+    )
+    true_crystal_array_list = get_crystals_list(
+        recon["input_data_batch"].frac_coords,
+        recon["input_data_batch"].atom_types,
+        recon["input_data_batch"].lengths,
+        recon["input_data_batch"].angles,
+        recon["input_data_batch"].num_atoms,
+    )
+    recon_list = [
+        Atoms(
+            c['atom_types'],
+            scaled_positions=c['frac_coords'],
+            cell=c['lengths'].tolist() + c['angles'].tolist(),
+        )
+        for c in crys_array_list
+    ]
+    true_list = [
+        Atoms(
+            c['atom_types'],
+            scaled_positions=c['frac_coords'],
+            cell=c['lengths'].tolist() + c['angles'].tolist(),
+        )
+        for c in true_crystal_array_list
+    ]
+
+    recon_dict = {
+        name: {"recon_atoms": recon_c, "true_atoms": true_c}
+        for name, recon_c, true_c in zip(
+            recon["input_data_batch"].mp_id, recon_list, true_list
+        )
+    }
+
+    with open(pkl, "wb") as f:
+        pickle.dump(recon_dict, f)
+    with open(idx, "w") as f:
+        for i, (name, recon_i) in enumerate(recon_dict.items()):
+            f.write("{:5d}    {}\n".format(i, name))
+            write(atoms_dir.joinpath(f"{i}.recon.vasp"), recon_i["recon_atoms"])
+            write(atoms_dir.joinpath(f"{i}.true.vasp"), recon_i["true_atoms"])
+
+
 @click.command
 @click.argument("recon_pt_file")
 def save_recon_structure(recon_pt_file):
     recon_pt_file = Path(recon_pt_file).resolve()
-    recon = torch.load(recon_pt_file)
-    print(recon.keys())
 
-    from eval_utils import get_crystals_list  # change working dir
-
-    batch = recon['input_data_batch']
-    print(batch)
-    true_crystal_array_list = get_crystals_list(
-        batch.frac_coords, batch.atom_types, batch.lengths,
-        batch.angles, batch.num_atoms)
-    print(true_crystal_array_list[0])
-    # c_list = get_crystals_list(
-    #     recon['frac_coords'][0],
-    #     recon['atom_types'][0],
-    #     recon['lengths'][0],
-    #     recon['angles'][0],
-    #     recon['num_atoms'][0],
-    # )
-    # a_list = [
-    #     Atoms(
-    #         c['atom_types'],
-    #         scaled_positions=c['frac_coords'],
-    #         cell=c['lengths'].tolist() + c['angles'].tolist(),
-    #     )
-    #     for c in tqdm(c_list)
-    # ]
-    # print(a_list[0])
+    extract_recon_pt(recon_pt_file)
 
 
 if __name__ == "__main__":
